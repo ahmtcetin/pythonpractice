681f1aaec57c61185cc3b35e1a29efe9860d64f1
ahmtcetin <ahmt.cetin@hotmail.com>
2025-06-27 18:29:34 +0000

    String operations

--- Section1/Script1.py
+++ Section1/Script1.py
@@ -1,1 +1,27 @@
 print("Hello, Fukcer!")
+
+A = "Hello"
+B = "Fucket"
+C = " "
+D = "!"
+result = A + C + B + 3 * D
+print(result)
+
+A = "Hello Fucker!!!"
+length = len(A)
+print("Length of A: ", length)
+
+upper = A.upper()
+lower = A.lower()
+print("UPPER: ", upper)
+print("LOWER: ", lower)
+
+B = A.replace("Fucker","Motherfucker")
+print(B)
+
+C = A.split( )
+print("Split Result: ",C)
+print(C[0])
+print(C[1])
+print(A.split( )[0])
+print(A.split( )[1])
